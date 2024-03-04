14ff653a48102b466873a6469715cc4bf532520d
masoodk <keshvari.developer@gmail.com>
2024-03-04 15:30:01 +0330

    feat: add more social media types

--- entity/social_media.go
+++ entity/social_media.go
@@ -8,17 +8,47 @@ type SocialMedia struct {
 type SocialMediasType int
 
 const (
-	SocialMediasTypeInstagram SocialMediasType = iota + 1
+	SocialMediasTypeFacebook SocialMediasType = iota + 1
 	SocialMediasTypeYoutube
+	SocialMediasTypeWhatsapp
+	SocialMediasTypeInstagram
+	SocialMediasTypeWechat
+	SocialMediasTypeTiktok
+	SocialMediasTypeSnapchat
+	SocialMediasTypeTwitter
+	SocialMediasTypeLinkedin
+	SocialMediasTypeReddit
+	SocialMediasTypePinterest
+	SocialMediasTypeTelegram
+	SocialMediasTypeDiscord
+	SocialMediasTypeTinder
+	SocialMediasTypeLine
+	SocialMediasTypeClubhouse
+	SocialMediasTypeTwitch
 )
 
 func (s SocialMediasType) IsValid() bool {
-	return s >= SocialMediasTypeInstagram && int(s) <= len(SocialMediasTypeStrings)
+	return s >= SocialMediasTypeFacebook && int(s) <= len(SocialMediasTypeStrings)
 }
 
 var SocialMediasTypeStrings = map[SocialMediasType]string{
-	SocialMediasTypeInstagram: "instagram",
+	SocialMediasTypeFacebook:  "facebook",
 	SocialMediasTypeYoutube:   "youtube",
+	SocialMediasTypeWhatsapp:  "Whatsapp",
+	SocialMediasTypeInstagram: "instagram",
+	SocialMediasTypeWechat:    "wechat",
+	SocialMediasTypeTiktok:    "tiktok",
+	SocialMediasTypeSnapchat:  "snapchat",
+	SocialMediasTypeTwitter:   "twitter",
+	SocialMediasTypeLinkedin:  "linkedin",
+	SocialMediasTypeReddit:    "reddit",
+	SocialMediasTypePinterest: "pinterest",
+	SocialMediasTypeTelegram:  "telegram",
+	SocialMediasTypeDiscord:   "discord",
+	SocialMediasTypeTinder:    "tinder",
+	SocialMediasTypeLine:      "line",
+	SocialMediasTypeClubhouse: "clubhouse",
+	SocialMediasTypeTwitch:    "twitch",
 }
 
 func (s SocialMediasType) String() string {
